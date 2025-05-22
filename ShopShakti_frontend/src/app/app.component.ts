6cae59d9838f453b4ef27335d6c74499bd014b21
Prathamesh Kasar <prathameshkasar000@gmail.com>
2025-05-22 12:58:23 +0530

    feat(homepage): create HomepageComponent and render it in AppComponent

--- ShopShakti_frontend/src/app/app.component.ts
+++ ShopShakti_frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ShopShakti_frontend/src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-homepage',
+  imports: [CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
