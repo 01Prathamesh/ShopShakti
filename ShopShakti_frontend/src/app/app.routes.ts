95140a0d3ebc4e735747a9c14a60da70af34eae5
Prathamesh Kasar <prathameshkasar000@gmail.com>
2025-05-23 13:52:53 +0530

    feat(router): define core application routes and implement lazy-loaded admin route with guard
    - Added routes for homepage, product list, product detail, login, register, profile, and cart components
    - Configured lazy loading for admin dashboard component with canActivateAdmin route guard for access control

--- ShopShakti_frontend/src/app/app.routes.ts
+++ ShopShakti_frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/core_pages/homepage/homepage.component';
+import { ProductListComponent } from './components/core_pages/product-list/product-list.component';
+import { ProductDetailComponent } from './components/core_pages/product-detail/product-detail.component';
+import { ProfileComponent } from './components/auth_user_pages/profile/profile.component';
+import { CartComponent } from './components/core_pages/cart/cart.component';
+import { RegisterComponent } from './components/auth_user_pages/register/register.component';
+import { LoginComponent } from './components/auth_user_pages/login/login.component';
+import { canActivateAdmin } from './components/admin/admin-dashboard/admin.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'cart', component: CartComponent},
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./components/admin/admin-dashboard/admin-dashboard.component').then(m => m.AdminDashboardComponent),
+    canActivate: [canActivateAdmin]
+  },
+];
